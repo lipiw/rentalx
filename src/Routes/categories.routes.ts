9757bd3c2d9eb75b6325f44c2354c59ec8acbc7c
Felipe Rafael Goncalves <fgoncalves@cpqd.com>
2022-03-08 23:22:29 -0300

    TDD

--- src/Routes/categories.routes.ts
+++ src/Routes/categories.routes.ts
@@ -1,18 +1,16 @@
 import { Router } from "express";
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
-    const categoryAlreadyExists = categoriesRepository.findByName(name);
 
-    if(categoryAlreadyExists){
-        return response.status(400).json({error: "Category Already exists!"});
-    }
+    const createCategory = new CreateCategoryService(categoriesRepository);
 
-    categoriesRepository.create({name, description});
+    createCategory.execute({name, description});
     
     return response.status(201).send();
 });
